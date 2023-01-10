89a2bdf831d5a0495e78175d3e8a29aa6b4bb992
carrizoja@gmail.com <carrizoja@gmail.com>
2023-01-10 17:00:45 -0300

    Segunda Entrega V1.2

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -1,30 +1,74 @@
 import { Injectable } from '@angular/core';
 import { Student } from '../models/student.model';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, take, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentsService {
+  private students = new BehaviorSubject<Student[]>([
+    new Student (1, 'Jose', 'Carrizo','carrizoja@gmail.com', true),
+    new Student (2, 'Sofi', 'Ceria', 'soficeria@outlook.com', false),
+    new Student (3, 'Walter', 'Leguizamon', 'walterleguizamon@live.com', true),
+    new Student (4, 'Raly', 'Barrionuevo', 'raly.barrionuevo@gmail.com', false),
+  ]);
 
-  constructor() { }
-
-  addStudent(student: Student): Observable<Student> {
-    return of(student);
-
+  public students$: Observable<Student[]>;
+  constructor() { 
+    this.students$ = this.students.asObservable();
   }
 
-  public studentsList(): Observable<Student[]> {
-    return of([
-      new Student (1, 'Jose', 'Carrizo','carrizoja@gmail.com', true),
-      new Student (2, 'Sofi', 'Ceria', 'soficeria@outlook.com', false),
-      new Student (3, 'Walter', 'Leguizamon', 'walterleguizamon@live.com', true),
-      new Student (4, 'Ciro', 'Pertusi', 'ciro.pertusi@gmail.com', false),
-    
-    ]);  
+ AddStudent (newStudentData: Omit<Student, 'id' | 'isActive'>): void {
+   this.students.pipe(take(1)).subscribe((students) => {
+      const lastId = students[students.length - 1]?.id || 0;
+      this.students.next([...students, new Student(lastId + 1, newStudentData.firstName, newStudentData.lastName, newStudentData.email, true)])
+   })
   }
 
+EditStudent(id: number, data:Partial<Student>): void {
+  this.students.pipe(take(1)).subscribe((students) => {
+    this.students.next(students.map(
+      (s) => s.id === id
+      ? new Student (
+        s.id,
+        data.firstName || s.firstName,
+        data.lastName || s.lastName,
+        data.email || s.email,
+        data.isActive || s.isActive
+      )
+      : s
+    )
+    )
+    })
+  }
 
- 
+  removeStudent(id:number): void {
+    this.students.pipe(take(1)).subscribe((students) => {
+      this.students.next(students.filter((s) => s.id !== id))
+    })
+  }
+
+  changeActive(student: Student): void {
+    this.students.pipe(take(1)).subscribe((students) => {
+      this.students.next(students.map((s) => {
+        if (s.id === student.id) {
+          return new Student(s.id, s.firstName, s.lastName, s.email, !s.isActive)
+        }
+        return s
+      }))
+    })
+  }
+
+  getStudentById(id: number): Observable<Student | null> {
+return this.students$.pipe(
+  take(1),
+  map((students) => students.find((s) => s.id === id) || null)
+)
+  }
+
 
 }
+
+  
+
+
